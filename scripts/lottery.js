7235dcf8180f53c1a4b82ab914a6c34932c2a9ab
rajkharvar <rajkharvar1808@gmail.com>
2022-04-19 17:46:34 +0530

    Minor update to Lottery contract

--- scripts/lottery.js
+++ scripts/lottery.js
@@ -1,10 +1,11 @@
 const hre = require("hardhat");
 
-const CONTRACT_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+const CONTRACT_ADDRESS = "0xb1Bc0fA8325b57B8bB4F0F77465A2a170A02B6c0";
 
 async function main() {
   const Lottery = await hre.ethers.getContractFactory("Lottery");
   let lottery = Lottery.attach(CONTRACT_ADDRESS);
+  let tx;
 
   const poolCounter = await lottery.poolCounter();
   console.log("poolCounter");
@@ -13,15 +14,19 @@ async function main() {
   const signers = await hre.ethers.getSigners();
 
   // Contribute with 5 different signer
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 0; i <= 4; i++) {
+    console.log(`Sending tx from signer ${i + 1} - ${signers[i].address}`);
     lottery = lottery.connect(signers[i]);
     tx = await lottery.contribute({
       value: hre.ethers.utils.parseEther("0.1"),
     });
+    await tx.wait();
   }
 
   // declare winner
+  console.log("Declaring winner");
   tx = await lottery.declareWinner();
+  await tx.wait();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
