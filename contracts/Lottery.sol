b36cd77d011e47f18684d856c2550c0f6a70501d
rajkharvar <rajkharvar1808@gmail.com>
2022-04-19 14:36:58 +0530

    Use RandomNoManager function for number generation

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,12 +6,14 @@ import "hardhat/console.sol";
 
 contract Lottery {
     uint256 public contributionAmount = 0.1 ether;
-    uint8 public immutable maxParticipants = 5;
+    uint8 public constant MAX_PARTICIPANTS = 5;
+    uint16 public constant EPOCH_LENGTH = 1800;
     uint256 public poolCounter = 1;
     uint8 public poolParticipantCounter = 0;
 
     mapping(uint256 => address[]) poolParticipants;
     mapping(uint256 => mapping(address => bool)) poolRecords;
+    mapping(uint256 => bool) epoches;
 
     IRandomNoManager public randomNoManager;
 
@@ -25,7 +27,7 @@ contract Lottery {
             "Already contributed to pool"
         );
         require(
-            poolParticipantCounter != maxParticipants,
+            poolParticipantCounter != MAX_PARTICIPANTS,
             "Pool reached max participants"
         );
         require(
@@ -40,16 +42,21 @@ contract Lottery {
 
     function declareWinner() public returns (address) {
         require(
-            poolParticipantCounter == maxParticipants,
+            poolParticipantCounter == MAX_PARTICIPANTS,
             "Max participants limit not reached"
         );
-        // Needs to convert this to random number generator function
-        uint256 index = block.timestamp % poolParticipants[poolCounter].length;
+        uint32 epoch = (uint32(block.timestamp) / (EPOCH_LENGTH));
+        require(!epoches[epoch], "Pool closed for current epoch");
+
+        uint256 index = randomNoManager.getGenericRandomNumberOfLastEpoch() %
+            MAX_PARTICIPANTS;
         address winner = poolParticipants[poolCounter][index];
         bool sent = payable(winner).send(0.5 ether);
         require(sent, "Failed to send reward");
+
         poolCounter++;
         poolParticipantCounter = 0;
+        epoches[epoch] = true;
         return winner;
     }
 
