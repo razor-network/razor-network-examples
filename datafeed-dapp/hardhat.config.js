918e00515c77a6e675b7ff47c22dd9053adf90d6
rajkharvar <rajkharvar1808@gmail.com>
2022-05-31 17:05:47 +0530

    Added skale config in hardhat.config.js

--- datafeed-dapp/hardhat.config.js
+++ datafeed-dapp/hardhat.config.js
@@ -1,14 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const PROVIDER_URL = process.env.PROVIDER_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +12,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    skaleTestnetV2: {
+      url: PROVIDER_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
