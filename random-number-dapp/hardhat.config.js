08ecd9f67cb9022e0728b0559f6dc96fb46033e7
rajkharvar <rajkharvar1808@gmail.com>
2022-06-08 12:32:44 +0530

    Add skale testnet v2 config in hardhat

--- random-number-dapp/hardhat.config.js
+++ random-number-dapp/hardhat.config.js
@@ -11,5 +11,9 @@ module.exports = {
       url: process.env.PROVIDER_URL || "",
       accounts: [process.env.PRIVATE_KEY],
     },
+    skaleTestnetV2: {
+      url: process.env.SKALE_TESTNET_V2_URL || "",
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
